c8c676c63971b392dd70b7318368c9d12dd55714
jonvikrr <studiesec.hula878@passmail.net>
2025-03-22 14:23:20 -0300

    docs: add code examples and notes for IO operations

---
+++ code-examples/000.basics/004.io-operations.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math"
+)
+
+func main() {
+	const inflationRate = 2.5
+	var investmentAmount, expectedReturnRate, years float64
+
+	fmt.Print("Investment amount: ")
+	fmt.Scan(&investmentAmount)
+
+	fmt.Print("Expected return rate: ")
+	fmt.Scan(&expectedReturnRate)
+
+	fmt.Print("Years: ")
+	fmt.Scan(&years)
+
+	futureValue := investmentAmount	* math.Pow(1 + expectedReturnRate / 100, years)
+	futureValue = futureValue / math.Pow(1 + inflationRate / 100, years)
+
+	fmt.Println("The future value would be:", futureValue)
+}
